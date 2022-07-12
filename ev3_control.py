949a874156ff6174cf6745d1b421d6754a711dd4
Milan Theiß <milan.theiss@gmx.net>
2022-07-12 12:03:29 +0200

    EV3 dreht sich zu Licht und stopt

--- ev3_control.py
+++ ev3_control.py
@@ -4,6 +4,7 @@ import threading
 from time import sleep
 
 from ev3dev2.motor import Motor, MoveJoystick, MoveTank
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sensor import INPUT_1, INPUT_2
 
@@ -11,29 +12,48 @@ logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(thr
 logger = logging.getLogger('EV3 CONTROLLER')
 logger.setLevel(logging.DEBUG)
 
-#motors = MoveJoystick("outD", "outA")
-#movetank = MoveTank("outD", "outA")
+#Linker Motor --> outB
+motorB = Motor(OUTPUT_B)
+#Rechte Motor --> outA
+motorA = Motor(OUTPUT_A)
 
-#motorD = Motor("outD")
-#motorA = Motor("outA")
+motors = MoveJoystick(OUTPUT_B, OUTPUT_A)
+movetank = MoveTank(OUTPUT_B, OUTPUT_A)
 
-lightsensor1 = ColorSensor(INPUT_1)
+#Linker Sensor --> in2
 lightsensor2 = ColorSensor(INPUT_2)
+#Rechter Sensor --> in1
+lightsensor1 = ColorSensor(INPUT_1)
 
 lightsensor1.MODE_COL_AMBIENT
-lightsensor1.MODE_COL_AMBIENT 
+lightsensor2.MODE_COL_AMBIENT 
 
 def followTheLight():
-    value1 = lightsensor1.ambient_light_intensity
-    value2 = lightsensor2.ambient_light_intensity
+    #Get Light Intensity
+    valueRight = lightsensor1.ambient_light_intensity
+    valueLeft = lightsensor2.ambient_light_intensity
 
-    logger.info('Links: {}'.format(value1))
-    logger.info('Rechts: {}'.format(value2))
+    valueDiff = valueRight - valueLeft
+
+    if valueDiff > 6:
+        movetank.on(35, -35)
+        logger.debug("Turning Right")
+        sleep(0.5)
+
+    if valueDiff < -6:
+        movetank.on(-35, 35)
+        logger.debug("Turning Left")
+        sleep(0.5)
+
+    movetank.off()
+    
+    logger.debug('Links: %d', valueRight)
+    logger.debug('Rechts: %d', valueLeft)
+    logger.debug('Value Diff: %d', valueDiff)
 
 
 if __name__ == '__main__':
     while True:
         followTheLight()
-        sleep(1)
 
     # Change checking for inputs to false to stop handleing --> bei Command
