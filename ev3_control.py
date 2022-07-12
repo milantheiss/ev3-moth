b5f597bd0bfb6a035b9590f3f102f104ab02359b
Noah Alps <noah.alps2003@gmail.com>
2022-07-12 12:43:15 +0200

    Ultraschall Sensor hinzugefügt

--- ev3_control.py
+++ ev3_control.py
@@ -4,28 +4,28 @@ from time import sleep
 
 from ev3dev2.motor import MoveTank
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B
-from ev3dev2.sensor.lego import ColorSensor
-from ev3dev2.sensor import INPUT_1, INPUT_2
+from ev3dev2.sensor.lego import ColorSensor, UltrasonicSensor
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s")
 logger = logging.getLogger('EV3 CONTROLLER')
 logger.setLevel(logging.DEBUG)
 
 # Linker Motor --> outB
-motorB = Motor(OUTPUT_B)
 # Rechte Motor --> outA
-motorA = Motor(OUTPUT_A)
-
-motors = MoveJoystick(OUTPUT_B, OUTPUT_A)
 movetank = MoveTank(OUTPUT_B, OUTPUT_A)
 
 # Linker Sensor --> in2
 lightsensor2 = ColorSensor(INPUT_2)
 # Rechter Sensor --> in1
 lightsensor1 = ColorSensor(INPUT_1)
+# Ultraschallsensor --> in3
+ultrasonicSensor = UltrasonicSensor(INPUT_3)
 
 lightsensor1.MODE_COL_AMBIENT
-lightsensor2.MODE_COL_AMBIENT 
+lightsensor2.MODE_COL_AMBIENT
+
+ultrasonicSensor.MODE_US_SI_CM
 
 def followTheLight():
     # Get Light Intensity
@@ -34,6 +34,9 @@ def followTheLight():
 
     # Calc Difference
     valueDiff = valueRight - valueLeft
+
+    # Distanz zu Objekten im Raum
+    distance = ultrasonicSensor.distance_centimeters
 
     # Wenn mehr Licht auf der rechten Seite ist, dreht sich der EV3 nach rechts
     if valueDiff > 6:
@@ -49,7 +52,15 @@ def followTheLight():
 
     # Motoren werden ausgeschaltet
     movetank.off()
-    
+
+    # Wenn Licht vor EV3, dann fahre vorwärts
+    if -5 <= valueDiff <= 5 and distance > 10:
+        movetank.on(70, 70)
+        logger.debug("Moving to Light")
+
+    # Motoren werden ausgeschaltet
+    movetank.off()
+
     logger.debug('Links: %d', valueRight)
     logger.debug('Rechts: %d', valueLeft)
     logger.debug('Value Diff: %d', valueDiff)
